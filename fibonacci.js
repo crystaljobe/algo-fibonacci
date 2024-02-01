1ea558a1051fd41eda473019717b0d460f33701a
crystaljobe <crystaljobe29@gmail.com>
2024-01-31 19:27:19 -0700

    first commit

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
-  return 0;
+  let fibonacciArr = [0, 1];
+
+  for (var i = 0; i < num; i++) {
+    fibonacciArr.push(fibonacciArr[i] + fibonacciArr[i+1]);
+  }
+
+  return fibonacciArr[num];
 }
+
+console.log(fibonacci(7));
 
 module.exports = fibonacci;
